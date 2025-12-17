9c06c715726cc5d47aa6f6e3b19616be9f8f0b1f
ericnetplus <ericnetplus@gmail.com>
2025-12-17 00:14:15 -0600

    deployed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/portfolio/',
+    base: '/portafolio/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
